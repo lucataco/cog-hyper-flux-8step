3499ab1b06cc1e1b2164b5b3b417e820aac593da
lucataco <me@lucataco.com>
2024-08-28 17:30:04 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# ByteDance/Hyper-SD FLUX.1-dev 8-step Cog Model
+
+This is an implementation of [ByteDance/Hyper-SD FLUX.1-dev 8-step](https://huggingface.co/ByteDance/Hyper-SD) as a [Cog](https://github.com/replicate/cog) model.
+
+## Development
+
+Follow the [model pushing guide](https://replicate.com/docs/guides/push-a-model) to push your own model to [Replicate](https://replicate.com).
+
+
+## How to use
+
+Make sure you have [cog](https://github.com/replicate/cog) installed.
+
+To run a prediction:
+
+    cog predict -i prompt="a dog smiling and looking directly at the camera, wearing a white t-shirt with the word 'HYPER' printed on it."
+
+![Output](output.0.png)
---
+++ predict.py
@@ -0,0 +1,190 @@
+# Prediction interface for Cog ⚙️
+# https://github.com/replicate/cog/blob/main/docs/python.md
+
+from cog import BasePredictor, Input, Path
+import os
+import time
+import torch
+import subprocess
+import numpy as np
+from typing import List
+from diffusers import FluxPipeline
+from transformers import CLIPImageProcessor
+from diffusers.pipelines.stable_diffusion.safety_checker import (
+    StableDiffusionSafetyChecker
+)
+
+MODEL_CACHE = "Hyper-FLUX.1-dev"
+MODEL_URL = "https://weights.replicate.delivery/default/ByteDance/Hyper-FLUX.1-dev-8steps/model.tar"
+SAFETY_CACHE = "safety-cache"
+FEATURE_EXTRACTOR = "/src/feature-extractor"
+SAFETY_URL = "https://weights.replicate.delivery/default/sdxl/safety-1.0.tar"
+
+ASPECT_RATIOS = {
+    "1:1": (1024, 1024),
+    "16:9": (1344, 768),
+    "21:9": (1536, 640),
+    "3:2": (1216, 832),
+    "2:3": (832, 1216),
+    "4:5": (896, 1088),
+    "5:4": (1088, 896),
+    "3:4": (896, 1152),
+    "4:3": (1152, 896),
+    "9:16": (768, 1344),
+    "9:21": (640, 1536),
+}
+
+def download_weights(url, dest):
+    start = time.time()
+    print("downloading url: ", url)
+    print("downloading to: ", dest)
+    subprocess.check_call(["pget", "-xf", url, dest], close_fds=False)
+    print("downloading took: ", time.time() - start)
+
+def make_multiple_of_16(x):
+    return (x + 15) // 16 * 16
+
+class Predictor(BasePredictor):
+    def setup(self) -> None:
+        """Load the model into memory to make running multiple predictions efficient"""
+        start = time.time()
+        os.environ["TRANSFORMERS_OFFLINE"] = "1"
+
+        print("Loading safety checker...")
+        if not os.path.exists(SAFETY_CACHE):
+            download_weights(SAFETY_URL, SAFETY_CACHE)
+        self.safety_checker = StableDiffusionSafetyChecker.from_pretrained(
+            SAFETY_CACHE, torch_dtype=torch.float16
+        ).to("cuda")
+        self.feature_extractor = CLIPImageProcessor.from_pretrained(FEATURE_EXTRACTOR)
+
+        print("Loading Flux txt2img Pipeline")
+        if not os.path.exists(MODEL_CACHE):
+            download_weights(MODEL_URL, MODEL_CACHE)
+        self.txt2img_pipe = FluxPipeline.from_pretrained(
+            MODEL_CACHE,
+            torch_dtype=torch.bfloat16
+        ).to("cuda")
+        print("setup took: ", time.time() - start)
+
+    @torch.amp.autocast('cuda')
+    def run_safety_checker(self, image):
+        safety_checker_input = self.feature_extractor(image, return_tensors="pt").to("cuda")
+        np_image = [np.array(val) for val in image]
+        image, has_nsfw_concept = self.safety_checker(
+            images=np_image,
+            clip_input=safety_checker_input.pixel_values.to(torch.float16),
+        )
+        return image, has_nsfw_concept
+
+    def aspect_ratio_to_width_height(self, aspect_ratio: str) -> tuple[int, int]:
+        return ASPECT_RATIOS[aspect_ratio]
+
+    @torch.inference_mode()
+    def predict(
+        self,
+        prompt: str = Input(description="Prompt for generated image"),
+        aspect_ratio: str = Input(
+            description="Aspect ratio for the generated image. The size will always be 1 megapixel, i.e. 1024x1024 if aspect ratio is 1:1. To use arbitrary width and height, set aspect ratio to 'custom'.",
+            choices=list(ASPECT_RATIOS.keys()) + ["custom"],
+            default="1:1",
+        ),
+        width: int = Input(
+            description="Width of the generated image. Optional, only used when aspect_ratio=custom. Must be a multiple of 16 (if it's not, it will be rounded to nearest multiple of 16)",
+            ge=256,
+            le=1440,
+            default=None,
+        ),
+        height: int = Input(
+            description="Height of the generated image. Optional, only used when aspect_ratio=custom. Must be a multiple of 16 (if it's not, it will be rounded to nearest multiple of 16)",
+            ge=256,
+            le=1440,
+            default=None,
+        ),
+        num_outputs: int = Input(
+            description="Number of images to output.",
+            ge=1,
+            le=4,
+            default=1,
+        ),
+        num_inference_steps: int = Input(
+            description="Number of inference steps",
+            ge=1,le=30,default=8,
+        ),
+        guidance_scale: float = Input(
+            description="Guidance scale for the diffusion process",
+            ge=0,le=10,default=3.5,
+        ),
+        seed: int = Input(description="Random seed. Set for reproducible generation", default=None),
+        output_format: str = Input(
+            description="Format of the output images",
+            choices=["webp", "jpg", "png"],
+            default="webp",
+        ),
+        output_quality: int = Input(
+            description="Quality when saving the output images, from 0 to 100. 100 is best quality, 0 is lowest quality. Not relevant for .png outputs",
+            default=80,
+            ge=0,
+            le=100,
+        ),
+        disable_safety_checker: bool = Input(
+            description="Disable safety checker for generated images. This feature is only available through the API. See [https://replicate.com/docs/how-does-replicate-work#safety](https://replicate.com/docs/how-does-replicate-work#safety)",
+            default=False,
+        ),
+    ) -> List[Path]:
+        """Run a single prediction on the model"""
+        if seed is None:
+            seed = int.from_bytes(os.urandom(2), "big")
+        print(f"Using seed: {seed}")
+
+        if aspect_ratio == "custom":
+            if width is None or height is None:
+                raise ValueError(
+                    "width and height must be defined if aspect ratio is 'custom'"
+                )
+            width = make_multiple_of_16(width)
+            height = make_multiple_of_16(height)
+        else:
+            width, height = self.aspect_ratio_to_width_height(aspect_ratio)
+        max_sequence_length = 512
+
+        flux_kwargs = {}
+        print(f"Prompt: {prompt}")
+        print("txt2img mode")
+        flux_kwargs["width"] = width
+        flux_kwargs["height"] = height
+        pipe = self.txt2img_pipe
+
+        generator = torch.Generator("cuda").manual_seed(seed)
+
+        common_args = {
+            "prompt": [prompt] * num_outputs,
+            "guidance_scale": guidance_scale,
+            "generator": generator,
+            "num_inference_steps": num_inference_steps,
+            "max_sequence_length": max_sequence_length,
+            "output_type": "pil"
+        }
+
+        output = pipe(**common_args, **flux_kwargs)
+
+        if not disable_safety_checker:
+            _, has_nsfw_content = self.run_safety_checker(output.images)
+
+        output_paths = []
+        for i, image in enumerate(output.images):
+            if not disable_safety_checker and has_nsfw_content[i]:
+                print(f"NSFW content detected in image {i}")
+                continue
+            output_path = f"/tmp/out-{i}.{output_format}"
+            if output_format != 'png':
+                image.save(output_path, quality=output_quality, optimize=True)
+            else:
+                image.save(output_path)
+            output_paths.append(Path(output_path))
+
+        if len(output_paths) == 0:
+            raise Exception("NSFW content detected. Try running it again, or try a different prompt.")
+
+        return output_paths
+    
